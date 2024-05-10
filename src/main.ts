90665c86e95f369c4eb63659e45b833745d860ba
Angel Martin <angelmb.dev@gmail.com>
2024-05-10 11:53:41 +0200

    feat: Initialize GitHub Action for branch name validation

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+
+async function run(): Promise<void> {
+    try {
+        const token = core.getInput('token', { required: true })
+        const patterns = core.getInput('patterns', { required: true }).split(',').map(s => s.trim());
+
+        // Crea una instancia del cliente de GitHub con el token proporcionado
+        const octokit = github.getOctokit(token);
+
+        const ref = github.context.ref
+        console.log(ref);
+
+
+    }
+    catch (err: any) {
+        core.setFailed(err.message)
+    }
+}
+
+run()
