783cfbb1046244fa299a70c0820e0f95782c7a11
Angel Martin <angelmb.dev@gmail.com>
2024-05-10 12:59:36 +0200

    feat: update token key

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import * as github from '@actions/github'
 
 async function run(): Promise<void> {
     try {
-        const token = core.getInput('token', { required: true })
+        const token = core.getInput('github_token', { required: true })
         const patterns = core.getInput('patterns', { required: true }).split(',').map(s => s.trim());
 
         // Crea una instancia del cliente de GitHub con el token proporcionado
